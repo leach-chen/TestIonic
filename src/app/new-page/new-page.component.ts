5a09fb0e8cb5d78122fb312d6a47135839b9be53
leach-chen <349763031@qq.com>
2020-02-18 22:58:37 +0800

    modify

--- src/app/new-page/new-page.component.ts
+++ src/app/new-page/new-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'new-page',
     templateUrl: './new-page.component.html',
-    styleUrls: ['./new-page-component.css']
+    styleUrls: ['./new-page.component.css']
 })
 
 export class NewPageComponent {
